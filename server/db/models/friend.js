6f45a4090bce24c5dcce583981f8d98a04abdd0b
VitaLiNaLi <vitailina5@gmail.com>
2023-12-08 12:16:41 +0300

    base

--- server/db/models/friend.js
+++ server/db/models/friend.js
@@ -7,20 +7,20 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate() {
-      // this.belongsTo(User, { foreignKey: "userId" });
+    static associate({User}) {
+      this.belongsTo(User, { foreignKey: "userId" });
     }
   }
   Friend.init(
     {
-      // userId: {
-      //   type: DataTypes.INTEGER,
-      //   references: {
-      //     model: "Users",
-      //     key: "id",
-      //   },
-      //   onDelete: "cascade",
-      // },
+      recipientId: {
+      type: DataTypes.INTEGER,
+      references: {
+      model: "Users",
+      key: "id",
+      },
+      onDelete: "cascade",
+    },
     },
     {
       sequelize,
